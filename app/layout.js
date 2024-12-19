f8ab2340a4e1c66a0a4581de1a652b6fdf0be29c
brian.musili <brian.musili@systechafrica.com>
2024-12-19 11:04:45 +0300

    Landing home page

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,38 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import Navigation from "./_components/Navigation";
 import Logo from "./_components/Logo";
 import "./_styles/globals.css";
+import {Josefin_Sans} from "next/font/google"
 
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export const metadata = {
   title: {
-    template: "%s - The Wild Oasis ",
+    template: "%s - The Wild Oasis",
     default: "The Wild Oasis",
   },
-  description: {
-    default: "A place to find your inner peace",
-  },
+  description: "A place to find your inner peace",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-      className="bg-primary-950 text-primary-100 min-h-screen"
+        className={`bg-primary-950 text-primary-100 min-h-screen antialiased  ${josefin.className} flex flex-col`}
       >
-        <header>
-          <Logo/>
-        <Navigation />
+        <header className="border-b border-primary-900 px-8 py-5">
+          <div className="flex justify-between items-center max-w-7xl mx-auto">
+            <Logo />
+            <Navigation />
+          </div>
         </header>
-        <main>{children}</main>
-        <footer>
-          Copyright by the Wild Oasis
-        </footer>
+        <div className="flex-1 px-8 py-12">
+
+        <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
+       
       </body>
     </html>
   );
